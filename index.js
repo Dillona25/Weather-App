b625eb2a685a08e2c45a295edcb9b4290c9d2b43
Dillona25 <Dillyb00by49@gmail.com>
2023-08-15 18:27:07 -0500

    Added new unit of measurement

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const apiKey = "feb4c18ca4cffb957bfa4ba9d695abed";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
 const searchBox = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-button");
