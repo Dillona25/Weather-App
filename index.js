cc9814fde5ccfd98e531dbeb862db3795f9827a5
Dillona25 <Dillyb00by49@gmail.com>
2023-08-15 18:24:50 -0500

    Added JS and API functionality

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const apiKey = "feb4c18ca4cffb957bfa4ba9d695abed";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search-input");
+const searchButton = document.querySelector(".search-button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "mph";
+}
+
+searchButton.addEventListener("click", (evt) => {
+  checkWeather(searchBox.value);
+  evt.preventDefault();
+});
